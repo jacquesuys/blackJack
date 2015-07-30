7ab4d8b0e47a2bd13e514a2a31b6f2ac1b7a4c3a
Jacques Uys <jaxuys@gmail.com>
2015-07-30 08:15:21 +0200

    Some changes to black Jack.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var blackJack = (function() {
+    "use strict";
+
     var win = 21,
         deck = [],
         //////   Hearts,   Diamonds, Clubs,    Spades 
         suits = ['\u2661', '\u2662', '\u2667', '\u2664'],
         rank = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'],
         player = {},
-        dealer = [];
+        dealer = {};
 
     function make() {
         suits.forEach(function(i) {
@@ -44,11 +46,39 @@ var blackJack = (function() {
 
     function deal() {
         player.hand = deck.splice(0, 2);
-        dealer = deck.splice(0, 2);
-        console.log(deck.length);
+        dealer.hand = deck.splice(0, 2);
+        console.log(addUp());
     }
 
     function hit() {
+    	var card = deck.splice(0, 1);
+    	player.hand.push(card[0]);
+    	console.log(addUp());
+    }
+
+    function addUp(obj) {
+    	var sum = 0, key;
+    	// maybe a reduce function?
+    	for(key in player.hand) {
+    		if(isNaN(player.hand[key].rank)) {
+    			if(player.hand[key].rank === 'A') {
+					(sum += 11 > 21) ? sum += 1 : sum += 11;
+				} else {
+					sum += 10;
+				}
+    		} else {
+    			sum += player.hand[key].rank;
+    		}
+    	}
+
+    	return sum;
+    }
+
+    function ifBust(val) {
+
+    }
+
+    function autoMate() {
 
     }
 
@@ -58,8 +88,7 @@ var blackJack = (function() {
     deal();
 
     return {
-        // after devving dont return the deck!!
-        deck: deck,
+        deck: deck, // remove afterwards
         deal: deal,
         hit: hit,
         shuffle: shuffle,
